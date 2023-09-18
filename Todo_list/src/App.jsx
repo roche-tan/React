183cda433146e1bc5b749a0c5c492daf081ac041
roche-tan <rochelyn.rallestan@gmail.com>
2023-09-18 13:49:13 +0200

    Todo list project

---
+++ Todo_list/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)} //this will appear in the input while you type
+            type="text"
+            id="item"
+          />
+        </div>
+        <button className="btn">Add </button>
+      </form>
+      <h1 className="header">Todo list</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
+}
